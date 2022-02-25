87657f169f37b3d93c07406d5107b9bff28584b8
drew <rostochek22@gmail.com>
2022-02-26 00:02:59 +0300

    change host for swagger

--- docs/docs.go
+++ docs/docs.go
@@ -805,7 +805,7 @@ type swaggerInfo struct {
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = swaggerInfo{
 	Version:     "1.0",
-	Host:        "127.0.0.1:3000",
+	Host:        "testing.rezero.pro",
 	BasePath:    "/api/v1",
 	Schemes:     []string{},
 	Title:       "Swagger Jameson API",
